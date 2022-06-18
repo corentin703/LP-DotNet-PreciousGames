797f69b577d5cd9b675880248b6a64b7a5ffbfda
Corentin VÉROT <corentin.verot@outlook.com>
2022-06-18 09:47:49 +0200

    Rebase - csproj

---
+++ Web/Models/EvaluationModels/EvaluationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using VerotMorin.PreciousGames.ModelLayer.Entities;
+using VerotMorin.PreciousGames.Web.Models.GameModels;
+
+namespace VerotMorin.PreciousGames.Web.Models.EvaluationModels
+{
+    public class EvaluationViewModel
+    {
+        public string Id { get; set; }
+        public string EvaluatorName { get; set; }
+        public DateTime Date { get; set; }
+        public float Mark { get; set; }
+        public GameViewModel Game { get; set; }
+
+        public EvaluationViewModel()
+        {
+            //
+        }
+
+        public EvaluationViewModel(Evaluation evaluation)
+        {
+            EvaluatorName = evaluation.EvaluatorName;
+            Date = evaluation.Date;
+            Mark = evaluation.Mark;
+            Game = new GameViewModel(evaluation.Game);
+        }
+    }
+}
---
+++ Web/Models/EvaluationModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace VerotMorin.PreciousGames.Web.Models.EvaluationModels
+{
+    public class IndexViewModel
+    {
+        public List<EvaluationViewModel> Evaluations { get; set; }
+        public int EvaluationCount { get; set; }
+    }
+}
